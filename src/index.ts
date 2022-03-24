e7a2b5880512c994a77f4d4839f5590363cfd084
zizaraDzr <elikdzr@mail.ru>
2022-03-24 22:18:06 +0300

    dynamical url
    50

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import './styles/main.css'
 import App from "./core/App";
 
 import { IStringArray } from './core/types/type'
+let locationPathname = window.location.pathname
+console.log(locationPathname);
+let idUrl = locationPathname === '/SPA-TS/' ? '/SPA-TS/' : '/'
 const menuItems:IStringArray = [
-    {id: '/', name: 'Главная'},
-    {id: 'dashboard', name: 'dashboard'},
-    {id: 'editor', name: 'editor'}
+    {id: `${idUrl}`, name: 'Главная'},
+    {id: `${idUrl}dashboard`, name: 'dashboard'},
+    {id: `${idUrl}editor`, name: 'editor'}
 ]
 
 new App(menuItems)
