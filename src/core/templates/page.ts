a17f65e2309d2865a1aae5ce6b3fe289acf38909
zizaraDzr <elikdzr@mail.ru>
2022-03-30 10:52:59 +0300

    Третий

--- src/core/templates/page.ts
+++ src/core/templates/page.ts
@@ -2,7 +2,7 @@ abstract class Page {
     protected container: HTMLElement;
     constructor(id: string) {
         this.container = document.createElement('div');
-        this.container.id = id
+        this.container.id = id;
     }
     setTitle(title: string) {
         document.title = title;
